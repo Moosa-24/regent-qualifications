148f08a0f25333b17b8e1e2442a127603733f463
Moosa Akoodie <moosaakoodie3@gmail.com>
2024-04-20 00:34:06 +0200

    mainly functionality changes, some css

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
+// Function to toggle article visibility
+function toggleArticleVisibility(article) {
+    let articles = document.querySelectorAll('article');
+    articles.forEach(function(article) {
+        article.classList.remove('show-article');
+    });
+    article.classList.toggle('show-article');
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    // Get all the anchor tags in the navigation
     let navLinks = document.querySelectorAll('.nav a');
 
-    // Loop through each anchor tag
     navLinks.forEach(function(link) {
-        // Add click event listener to each anchor tag
         link.addEventListener('click', function(event) {
-            // Prevent the default action of the anchor tag
             event.preventDefault();
-
-            // Get the ID of the article to display from the href attribute
-            let targetId = link.getAttribute('href').substring(1);
-
-            // Find the article with the corresponding ID
+            let targetId = this.getAttribute('href').substring(1);
             let targetArticle = document.getElementById(targetId);
 
-            // Scroll to the article
             if (targetArticle) {
-                targetArticle.scrollIntoView({ behavior: 'smooth' });
+                toggleArticleVisibility(targetArticle);
             }
         });
     });
 });
+
+// Check if the page is accessed via a link with an anchor
+if (window.location.hash) {
+    let articleId = window.location.hash.substring(1);
+    let article = document.getElementById(articleId);
+    if (article) {
+        toggleArticleVisibility(article);
+    }
+}
