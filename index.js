c1afd5a08f078d8f48acfcd0d192c7431a97ce4f
Moosa Akoodie <moosaakoodie3@gmail.com>
2024-04-20 14:54:13 +0200

    dark background css/javascript comments

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-// Function to toggle article visibility
+// Function to toggle the visibility of an article
 function toggleArticleVisibility(article) {
+    // Get all articles on the page
     let articles = document.querySelectorAll('article');
+    
+    // Hide all articles
     articles.forEach(function(article) {
         article.classList.remove('show-article');
     });
+    
+    // Show the clicked article
     article.classList.toggle('show-article');
 
-    // Show the close button
+    // Show the close button for the clicked article
     let closeButton = article.querySelector('.close-button');
     if (closeButton) {
         closeButton.style.display = 'block';
@@ -15,9 +20,13 @@ function toggleArticleVisibility(article) {
 
 // Function to close the image viewer
 function closeImageViewer() {
+    // Get all articles on the page
     let articles = document.querySelectorAll('article');
+    
+    // Hide all articles and their close buttons
     articles.forEach(function(article) {
         article.classList.remove('show-article');
+        
         // Hide the close button
         let closeButton = article.querySelector('.close-button');
         if (closeButton) {
@@ -27,14 +36,20 @@ function closeImageViewer() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    // Get all navigation links
     let navLinks = document.querySelectorAll('.nav a');
 
+    // Add click event listeners to all navigation links
     navLinks.forEach(function(link) {
         link.addEventListener('click', function(event) {
+            // Prevent the default link behavior
             event.preventDefault();
+            
+            // Get the target article's ID from the link's href attribute
             let targetId = this.getAttribute('href').substring(1);
             let targetArticle = document.getElementById(targetId);
 
+            // If the target article exists, toggle its visibility
             if (targetArticle) {
                 toggleArticleVisibility(targetArticle);
             }
@@ -43,8 +58,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Check if the page is accessed via a link with an anchor
     if (window.location.hash) {
+        // Get the ID of the article specified in the anchor
         let articleId = window.location.hash.substring(1);
         let article = document.getElementById(articleId);
+        
+        // If the article exists, toggle its visibility
         if (article) {
             toggleArticleVisibility(article);
         }
